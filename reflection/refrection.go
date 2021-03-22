c9cab42c41d0d7a400089cd43046da2767c13afb
generalworksinc <y.shinbori@generalworks.co.jp>
2021-03-22 22:51:50 +0900

    add reflection util

--- reflection/refrection.go
+++ reflection/refrection.go
@@ -11,11 +11,16 @@ func GetStructFields(st interface{}, isSnakeCase bool) []string {
 	t := v.Type()
 
 	for i := 0; i < t.NumField(); i++ {
-		// フィールド名
+		// フィールド名の取得
+		//フィールドがstructだった場合、再帰的に取得
+		if t.Field(i).Type.Kind() == reflect.Struct {
+			newInstance := reflect.New(t.Field(i).Type).Elem()
+			fields = append(fields, GetStructFields(newInstance, isSnakeCase)...)
+		}
 		if isSnakeCase {
-			fields = append(fields, t.Field(i).Name)
-		} else {
 			fields = append(fields, gw_strings.ToSnakeCase(t.Field(i).Name))
+		} else {
+			fields = append(fields, t.Field(i).Name)
 		}
 	}
 	return fields
