b0fb8947829ce894f03e3fd08efbd15cdc581331
generalworksinc <y.shinbori@generalworks.co.jp>
2021-04-15 13:10:25 +0900

    add Set struct

--- common/common.go
+++ common/common.go
@@ -7,3 +7,33 @@ func CpInt(cond bool, a int, b int) int {
 		return b
 	}
 }
+
+type Set struct {
+	dataMap map[interface{}]string
+}
+
+func (set *Set) Put(key interface{}) {
+	set.dataMap[key] = ""
+}
+func (set *Set) Values(key interface{}) []interface{} {
+	keys := []interface{}{}
+	for k, _ := range set.dataMap {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+type StringSet struct {
+	dataMap map[string]string
+}
+
+func (set *StringSet) Put(key string) {
+	set.dataMap[key] = ""
+}
+func (set *StringSet) Values(key string) []string {
+	keys := []string{}
+	for k, _ := range set.dataMap {
+		keys = append(keys, k)
+	}
+	return keys
+}
