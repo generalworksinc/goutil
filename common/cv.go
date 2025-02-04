c1c00801cd249ad1768cc5da3c4b33ee512656f1
myuna <masato.yuna1@gmail.com>
2025-02-04 15:43:23 +0900

    add clone funcs

--- common/cv.go
+++ common/cv.go
@@ -7,3 +7,34 @@ func Ip(i int) *int {
 func Pointer[T any](value T) *T {
 	return &value
 }
+
+func Clone[T any](value *T) *T {
+	if value == nil {
+		return nil
+	}
+	clone := *value
+	return &clone
+}
+
+func CloneSlice[T any](value []*T) []*T {
+	if value == nil {
+		return nil
+	}
+	clonedSlice := make([]*T, len(value))
+	for i, v := range value {
+		clonedSlice[i] = Clone(v)
+	}
+	return clonedSlice
+}
+
+func CloneMap[K comparable, V any](value map[K]*V) map[K]*V {
+	if value == nil {
+		return nil
+	}
+	clonedMap := make(map[K]*V, len(value))
+	for k, v := range value {
+		clonedMap[k] = Clone(v)
+
+	}
+	return clonedMap
+}
